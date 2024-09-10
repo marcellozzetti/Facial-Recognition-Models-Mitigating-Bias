0c53e865e9934f13a2c49e45c66dd04cb81889ba
marcellozzetti <48164092+marcellozzetti@users.noreply.github.com>
2024-09-09 23:48:43 -0300

    commit

--- main.py
+++ main.py
@@ -430,7 +430,7 @@ model = nn.DataParallel(model)  # Paraleliza o modelo entre várias GPUs
 
 #criterion = ArcFaceLoss()
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9, weight_decay=0.0005)
+optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9, weight_decay=0.0005)
 
 scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=30, gamma=0.1)
 
