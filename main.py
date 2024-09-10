d605fbab33185aa1113133243498a0f301f0c5af
marcellozzetti <48164092+marcellozzetti@users.noreply.github.com>
2024-09-09 22:12:13 -0300

    commit

--- main.py
+++ main.py
@@ -365,7 +365,6 @@ class FaceDataset(Dataset):
 
 # Transformations and normalization
 transform = transforms.Compose([
-    transforms.Resize((224, 224)),
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
 ])
