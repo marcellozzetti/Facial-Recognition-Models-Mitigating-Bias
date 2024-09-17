a6e335af10cea58c6e9bee07aaacbf03e7bbb213
Marcello Ozzetti <48164092+marcellozzetti@users.noreply.github.com>
2024-09-17 18:03:57 -0300

    Update main.py

--- main.py
+++ main.py
@@ -96,7 +96,7 @@ transform = transforms.Compose([
     #transforms.RandomHorizontalFlip(),
     #transforms.ToPILImage(),
     #transforms.Resize((224, 224)),
-    transforms.ToTensor(),
+    transforms.ToTensor()
     #transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 ])
 
