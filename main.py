288dcd7bb9233fdbfcd06059e2d14a4f4dda562e
Marcello Ozzetti <48164092+marcellozzetti@users.noreply.github.com>
2024-09-28 15:32:47 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,52 +1,42 @@
-#1. Realizar a instalação de bibliotecas adicionais e import no Python
-
 print("Step 1 (Imports): Starting")
 
+import os
+import time
+import json
+import math
+import ssl
+import gc
+import io
+import zipfile
+import requests
+import numpy as np
 import pandas as pd
 import seaborn as sns
-import numpy as npy
-
 import matplotlib.pyplot as plt
-
-import plotly.express as px
-import plotly.graph_objs as go
-
-import torch
-import torch.nn as nn
-import torch.optim as optim
-from torch.utils.data import DataLoader, Dataset, Subset, random_split, WeightedRandomSampler
-
-from torch.amp import GradScaler, autocast
-
+import matplotlib.patches as patches
+from matplotlib.patches import Rectangle, Circle
+from PIL import Image
+from scipy.spatial import distance
 from sklearn.metrics import accuracy_score, precision_score, log_loss
 from sklearn.utils import resample
 from sklearn.preprocessing import LabelEncoder
-
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader, Dataset, random_split
+from torch.optim.lr_scheduler import ReduceLROnPlateau
+import torch.nn.functional as F
 import torchvision.transforms as transforms
 import torchvision.models as models
 from torchvision.models import ResNet50_Weights
-from torch.optim.lr_scheduler import ReduceLROnPlateau
-import torch.nn.functional as F
-
-import matplotlib.patches as patches
-from matplotlib.patches import Rectangle, Circle
-from scipy.spatial import distance
-
-from mtcnn.mtcnn import MTCNN
-
-from PIL import Image
-
-import zipfile
-import requests
-import time
-import math
-import io
-import os
-import ssl
 import cv2
-import gc
-
+from mtcnn.mtcnn import MTCNN
 import pre_processing_images
+
+# Constants
+BATCH_SIZE = 128
+NUM_EPOCHS = 50
+LEARNING_RATES = [0.001, 0.0001, 0.00001]
 
 print("Step 1 (Imports): End")
 
@@ -69,22 +59,13 @@ class FaceDataset(Dataset):
 
         try:
             img = cv2.imread(img_name)
-            
             if img is None:
                 raise FileNotFoundError(f"Image {img_name} not found")
-
             img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
-            #results = detector.detect_faces(img)
-            #if len(results) == 0:
-            #    raise ValueError("No face detected")
-
             if self.transform:
                 img = Image.fromarray(img)
                 img = self.transform(img)
-
             return img, label
-
         except (FileNotFoundError, ValueError) as e:
             return None, None
 
@@ -92,22 +73,14 @@ class FaceDataset(Dataset):
 transform = transforms.Compose([
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
-    #transforms.RandomResizedCrop(224),
-    #transforms.RandomHorizontalFlip(),
-    #transforms.ToPILImage(),
-    #transforms.Resize((224, 224)),
-    #transforms.ToTensor()
-    #transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 ])
 
 csv_pd = pd.read_csv(pre_processing_images.csv_balanced_concat_dataset_file)
-
 dataset = FaceDataset(csv_pd, pre_processing_images.img_processed_dir, transform=transform)
 
 # Split dataset into training and validation sets
 train_size = int(0.8 * len(dataset))
 val_size = len(dataset) - train_size
-
 train_dataset, val_dataset = random_split(dataset, [train_size, val_size])
 
 def collate_fn(batch):
@@ -120,9 +93,8 @@ label_encoder = LabelEncoder()
 label_encoder.fit(csv_pd['race'])
 
 # Create DataLoaders using a filter function: collate_fn
-train_loader = DataLoader(train_dataset, batch_size=128, shuffle=True, num_workers=4, pin_memory=True, prefetch_factor=2, collate_fn=collate_fn)
-
-val_loader = DataLoader(val_dataset, batch_size=128, shuffle=False, num_workers=4, pin_memory=True, prefetch_factor=2, collate_fn=collate_fn)
+train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=4, pin_memory=True, prefetch_factor=2, collate_fn=collate_fn)
+val_loader = DataLoader(val_dataset, batch_size=BATCH_SIZE, shuffle=False, num_workers=4, pin_memory=True, prefetch_factor=2, collate_fn=collate_fn)
 
 class ArcMarginProduct(nn.Module):
     def __init__(self, in_features, out_features, s=30.0, m=0.50, easy_margin=False, ls_eps=0.0):
@@ -166,54 +138,35 @@ class LResNet50E_IR(nn.Module):
         x = self.dropout(x)
         return x
 
-#class LResNet50E_IR(nn.Module):
-#    def __init__(self, num_classes=len(label_encoder.classes_)):
-#        super(LResNet50E_IR, self).__init__()
-#        self.backbone = models.resnet50(weights=ResNet50_Weights.DEFAULT)
-#        self.dropout = nn.Dropout(p=0.5)  # Adding dropout to reduce overfitting
-#        self.fc = nn.Linear(self.backbone.fc.in_features, num_classes)
-#        self.backbone.fc = self.fc
-
-#    def forward(self, x):
-#        x = self.backbone(x)
-#        x = self.dropout(x)
-#        return x
-    
-# Adjustments learning Rate
 def adjust_learning_rate(optimizer, epoch):
     if epoch < 10:
-        lr = 0.001
+        lr = LEARNING_RATES[0]
     elif epoch < 15:
-        lr = 0.0001
+        lr = LEARNING_RATES[1]
     else:
-        lr = 0.00001
+        lr = LEARNING_RATES[2]
     for param_group in optimizer.param_groups:
         param_group['lr'] = lr
 
 # Initialize model, criterion, and optimizer
 model = LResNet50E_IR().to(pre_processing_images.device)
-model = nn.DataParallel(model)  # Paraleliza o modelo entre várias GPUs
+model = nn.DataParallel(model)
 
-#criterion = ArcFaceLoss()
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9, weight_decay=0.0005)
-#optimizer = torch.optim.Adam(model.parameters(), lr=0.001, weight_decay=1e-4)
-
 scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min', patience=3)
 
 def softmax(x):
-    exp_x = npy.exp(x - npy.max(x))
+    exp_x = np.exp(x - np.max(x))
     return exp_x / exp_x.sum(axis=1, keepdims=True)
 
-print("Step 10 (CNN model): End")
+print("Step 9 (CNN model): End")
 
-#11. Treinar e extrair as métricas
 
-print("Step 11 (Training execution): Start")
+print("Step 10 (Training execution): Start")
 
-num_epochs = 50
-
-# General Metrics
+# Training execution
+num_epochs = NUM_EPOCHS
 train_losses = []
 accuracies = []
 precisions = []
@@ -224,15 +177,12 @@ accumulation_steps = 4
 
 for epoch in range(num_epochs):
     adjust_learning_rate(optimizer, epoch)
-
     model.train()
     start_time = time.time()
     
     for images, labels in train_loader:
         images = images.to(pre_processing_images.device)
-
         labels_tensor = torch.tensor(label_encoder.transform(labels)).to(pre_processing_images.device)
-
         optimizer.zero_grad()
 
         if pre_processing_images.device == 'cuda':
@@ -252,10 +202,7 @@ for epoch in range(num_epochs):
             optimizer.step()
 
     overhead = time.time() - start_time
-
-    # Metrics for training step
-    print(f'Epoch {epoch+1}/{num_epochs}, '
-          f'Overhead: {overhead:.4f}s')
+    print(f'Epoch {epoch+1}/{num_epochs}, Overhead: {overhead:.4f}s')
 
     model.eval()
     all_labels = []
@@ -266,9 +213,7 @@ for epoch in range(num_epochs):
     with torch.no_grad():
         for images, labels in val_loader:
             images = images.to(pre_processing_images.device)
-
             labels_tensor = torch.tensor(label_encoder.transform(labels)).to(pre_processing_images.device)
-
             outputs = model(images)
             loss = criterion(outputs, labels_tensor)
             epoch_loss += loss.item()
@@ -276,43 +221,26 @@ for epoch in range(num_epochs):
             probs = F.softmax(outputs, dim=1).cpu().numpy()
             preds = torch.max(outputs, 1)[1].cpu().numpy()
 
-            #print("Outputs val:", outputs)
-            #print("Labels val:", labels_tensor)
-            #print("probs val:", probs)
-            #print("preds val:", preds)
-
             all_labels.extend(labels_tensor)
             all_preds.extend(preds)
             all_probs.extend(probs)
 
-    # Numpy conversion 
-    #all_labels = npy.array(all_labels)
-    #all_preds = npy.array(all_preds)
-    #all_probs = npy.array(all_probs)
     all_labels = [label.item() for label in all_labels]
-
-    #print("all_labels", all_labels)
-    #print("all_preds", all_preds)
-
     accuracy = accuracy_score(all_labels, all_preds)
     precision = precision_score(all_labels, all_preds, average='weighted', zero_division=0)
-
     all_probs = softmax(all_probs)
     logloss = log_loss(all_labels, all_probs)
 
     scheduler.step(epoch_loss)
 
-    # Armazenando as métricas
     train_losses.append(epoch_loss / len(val_loader))
     accuracies.append(accuracy)
     precisions.append(precision)
     log_losses.append(logloss)
 
-    # Metrics for validation step
     print(f'Epoch {epoch+1}/{num_epochs}, Loss: {epoch_loss/len(train_loader):.4f}, '
           f'Accuracy: {accuracy:.4f}, Precision: {precision:.4f}, Log Loss: {logloss:.4f}')
 
-    # Resources optimization
     del images, labels, outputs, loss
     gc.collect()
     if torch.cuda.is_available() and pre_processing_images.device == 'cuda':
@@ -321,14 +249,8 @@ for epoch in range(num_epochs):
 torch.save(model.state_dict(), pre_processing_images.model_fairface_file)
 print('Finished Training and Model Saved')
 
-print("train_losses", train_losses)
-print("accuracies", accuracies)
-print("precisions", precisions)
-print("log_losses", log_losses)
-
-# Ploting general metrics
+# Plotting general metrics
 epochs_range = range(1, num_epochs + 1)
-
 plt.figure(figsize=(12, 8))
 
 plt.subplot(2, 2, 1)
