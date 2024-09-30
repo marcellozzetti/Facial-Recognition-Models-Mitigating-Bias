b7814e8595388f03b724a109933d85cd30db735f
Marcello Ozzetti <48164092+marcellozzetti@users.noreply.github.com>
2024-09-30 15:37:40 -0300

    Update run_new_main.py

--- run_new_main.py
+++ run_new_main.py
@@ -106,7 +106,7 @@ scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer, max_lr=0.01, epochs=N
 
 scaler =  torch.amp.GradScaler(torch.device(device)) if device == torch.device("cuda") else None
 
-Print("scaler: ", scaler)
+print("scaler: ", scaler)
 
 # Training function
 def train_model(model, criterion, optimizer, scheduler, num_epochs):
