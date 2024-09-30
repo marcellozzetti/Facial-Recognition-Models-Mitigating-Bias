f2231192b7782dff020aa713332aeb55a41132fc
Marcello Ozzetti <48164092+marcellozzetti@users.noreply.github.com>
2024-09-30 08:09:21 -0300

    Create face_dataset.py

---
+++ face_dataset.py
@@ -0,0 +1,24 @@
+
+
+# Dataset customizado usando CSV para as labels
+class FaceDataset(Dataset):
+    def __init__(self, csv_pd, img_dir, transform=None):
+        self.labels_df = csv_pd
+        self.img_dir = img_dir
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.labels_df)
+
+    def __getitem__(self, idx):
+        img_name = os.path.join(self.img_dir, self.labels_df.iloc[idx, 0])
+        label = self.labels_df.iloc[idx, 3]  # Coluna com o rótulo 'race'
+
+        img = cv2.imread(img_name)
+        if img is None:
+            raise FileNotFoundError(f"Image {img_name} not found")
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        if self.transform:
+            img = Image.fromarray(img)
+            img = self.transform(img)
+        return img, label
