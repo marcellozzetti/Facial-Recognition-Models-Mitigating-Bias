dc65882835135c98133ae0b3868d7e5e423d689a
Marcello Ozzetti <48164092+marcellozzetti@users.noreply.github.com>
2024-10-02 13:31:15 -0300

    Update face_dataset.py

--- face_dataset.py
+++ face_dataset.py
@@ -68,7 +68,7 @@ def dataset_transformation(img):
         #transforms.RandomHorizontalFlip(),
         #transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.2),
         transforms.ToTensor(),
-        transforms.Resize((224, 224))
-        #transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        transforms.Resize((224, 224)),
+        transforms.Normalize(mean=[0.4299, 0.3433, 0.3033], std=[0.2393, 0.2142, 0.2048])
     ])
     return transform(img)
