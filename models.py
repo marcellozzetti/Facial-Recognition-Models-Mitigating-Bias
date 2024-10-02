b811a3ddcb4d7fff227d96f6e818ccaa5dd0a471
Marcello Ozzetti <48164092+marcellozzetti@users.noreply.github.com>
2024-10-02 15:46:06 -0300

    Update models.py

--- models.py
+++ models.py
@@ -109,3 +109,40 @@ class ArcFaceLoss(nn.Module):
         arcface_logits = arcface_logits * self.scale
         
         return F.cross_entropy(arcface_logits, labels)
+
+
+class ArcMarginProduct(nn.Module):
+    def __init__(self, in_features, out_features, s=30.0, m=0.50, easy_margin=False):
+        super(ArcMarginProduct, self).__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.s = s  # Escala
+        self.m = m  # Margem angular
+
+        self.weight = nn.Parameter(torch.Tensor(out_features, in_features))
+        nn.init.xavier_uniform_(self.weight)
+
+        self.easy_margin = easy_margin
+        self.cos_m = math.cos(m)
+        self.sin_m = math.sin(m)
+        self.th = math.cos(math.pi - m)
+        self.mm = math.sin(math.pi - m) * m
+
+    def forward(self, input, label):
+        # Normalizando input e pesos
+        cosine = F.linear(F.normalize(input), F.normalize(self.weight))
+        sine = torch.sqrt(1.0 - torch.pow(cosine, 2))
+        phi = cosine * self.cos_m - sine * self.sin_m  # Ajuste angular
+
+        if self.easy_margin:
+            phi = torch.where(cosine > 0, phi, cosine)
+        else:
+            phi = torch.where(cosine > self.th, phi, cosine - self.mm)
+
+        one_hot = torch.zeros(cosine.size(), device=input.device)
+        one_hot.scatter_(1, label.view(-1, 1).long(), 1)
+
+        output = (one_hot * phi) + ((1.0 - one_hot) * cosine)
+        output *= self.s  # Aplicando a escala
+
+        return output
