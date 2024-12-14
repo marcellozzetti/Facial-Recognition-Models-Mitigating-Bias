df3d8d009158ea60e11293640118d35408f390ed
Marcello Ozzetti <48164092+marcellozzetti@users.noreply.github.com>
2024-12-14 15:43:08 -0300

    Update tsne_analysis.py

--- tsne_analysis.py
+++ tsne_analysis.py
@@ -1,6 +1,7 @@
 import torch
 from sklearn.manifold import TSNE
 import numpy as np
+from tqdm import tqdm
 
 # Function to generate t-SNE visualization
 def generate_tsne_visualization(model, data_loader, label_encoder, arc_face_margin=None):
