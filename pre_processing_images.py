af0df896948869666a48a82ac60c14452258d996
Marcello Ozzetti <48164092+marcellozzetti@users.noreply.github.com>
2024-12-14 17:43:05 -0300

    Update pre_processing_images.py

--- pre_processing_images.py
+++ pre_processing_images.py
@@ -50,7 +50,7 @@ MAX_SAMPLES = 5000
 ssl._create_default_https_context = ssl._create_unverified_context
 
 # Initialize the MTCNN detector
-detector = MTCNN(device=torch.device('cpu'))
+detector = MTCNN().to(torch.device('cpu'))
 
 # Check if Cuda is available
 cuda_available = torch.cuda.is_available()
